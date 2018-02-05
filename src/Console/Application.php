ee5c4051359c78c670668f668bedb30c270d5713
John Aldrich Bernardo <4ldrich@protonmail.com>
2018-02-05 11:19:09 +0800

    SAPI checking

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -11,6 +11,10 @@ class Application
     private $_output;
 
     function __construct() {
+        if (php_sapi_name() !== 'cli') {
+            exit('Application must be run from CLI only.' . PHP_EOL);
+        }
+
         $this->_input = new \Console\Input();
         $this->_output = new \Console\Output();
     }
@@ -25,7 +29,7 @@ class Application
         
         if (isset($argv[1])) {
             $commandName = strtolower($argv[1]);
-            
+
             if (isset($this->_commands[$commandName])) {
                 $cmd = $this->_commands[$commandName];
                 $cmd($this->_input, $this->_output);
